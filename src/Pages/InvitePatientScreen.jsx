b92101b227560f0527059c59072f8ca7d28763f8
Finn Hoogerwerf <90393784+JuicyPotat0@users.noreply.github.com>
2023-10-23 00:37:38 +0100

    Can now write new invites to database.

--- src/Pages/InvitePatientScreen.jsx
+++ src/Pages/InvitePatientScreen.jsx
@@ -1,7 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../Contexts/actionCreators/ userActionCreator';
+import { doc, setDoc } from "firebase/firestore"; 
+import { db } from "../firebase";
+
 const InvitePatientScreen = () => {
+
+    const currentUser = useSelector((state) => state.auth.currentUser);
+
+    //const [senderID, setSenderID] = useState('');
+    const [receiverID, setReceiverID] = useState('');
+    const [message, setMessage] = useState('');
 
     const dispatch = useDispatch();
 
@@ -9,6 +19,35 @@ const InvitePatientScreen = () => {
         dispatch(getUsers());
     }, [dispatch]);
 
+    const handleInvite = async (e) => {
+        e.preventDefault();
+        try {
+            //if (invite to patient does not yet exist) {
+                await setDoc(doc(db, "patientInvites", "test" /*make this a random doc ID*/), {
+                    senderID: currentUser.uid,
+                    receiverID: receiverID,
+                    message: message
+                });
+
+                toast.success('Invite successfully sent!', {
+                    position: 'top-center',
+                    autoClose: 3000,
+                });
+            //}
+
+            /*else {
+                toast.error('An invite to this patient already exists...', {
+                    position: 'top-center',
+                    autoClose: 3000,
+                });
+            }*/
+        }
+        
+        catch (error) {
+          console.error('Error:', error);
+        }
+    };
+    
     return (
         <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl grid md:grid-cols-2 gap-4 shadow-lg">
             <div className="flex flex-col justify-between mt-4 md:mt-0 md:ml-4">
@@ -25,6 +64,7 @@ const InvitePatientScreen = () => {
                             type='id'
                             className='border rounded-md px-2 py-1 text-gray-800 col-span-2'
                             placeholder='<ID>'
+                            onChange = {(e) => setReceiverID(e.target.value)}
                         />
 
                         <label className='text-gray-600 font-medium text-sm uppercase' htmlFor='message'>
@@ -35,13 +75,14 @@ const InvitePatientScreen = () => {
                             type={'message'}
                             className='border rounded-md px-2 py-1 text-gray-800 col-span-2'
                             placeholder='<Message (Optional)>'
+                            onChange = {(e) => setMessage(e.target.value)}
                         />
                     </div>
                 </div>
 
                 <button
                     className='mt-4 px-4 py-2 bg-blue-500 text-white rounded-md'
-                    onClick={() => ""}
+                    onClick={handleInvite}
                 >
                     Send invite
                 </button>
